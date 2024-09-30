19fbc76b91b3c974e908bc951fdc92923b06ce8a
Grimm <rafalmirkowski@gmail.com>
2024-09-30 12:12:27 +0200

    f

--- apps/test-shad/src/routes/AppRoutes.tsx
+++ apps/test-shad/src/routes/AppRoutes.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { routes } from './routes.schema';
 import { Route, Routes } from 'react-router-dom';
-import {Button} from "@/components/ui/button";
 
 export const AppRoutes = (): React.ReactElement => {
   return (
@@ -9,7 +8,7 @@ export const AppRoutes = (): React.ReactElement => {
       <Route
         path={routes.BASE_ROUTE}
         element={
-            <Button> hello button </Button>
+          <div className={'h-screen text-black-500 text-2xl'}>hello there</div>
         }
       />
     </Routes>
--- apps/test-ui/tailwind.config.js
+++ apps/test-ui/tailwind.config.js
@@ -1,21 +1,61 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      // for me from future reference
-      // https://www.realtimecolors.com/dashboard?colors=0d0d17-f9f9fb-5f5da8-d2accd-b77b9b&fonts=Roboto-Roboto
-      colors: {
-        'text': '#0d0d17',
-        'background': '#f9f9fb',
-        'primary': '#5f5da8',
-        'secondary': '#d2accd',
-        'accent': '#b77b9b',
-      },
-    },
+  	extend: {
+  		colors: {
+  			text: '#0d0d17',
+  			background: 'hsl(var(--background))',
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 }
