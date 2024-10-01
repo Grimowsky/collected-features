4f75f029e8219d77cc0e239603671933764b7805
Grimm <rafalmirkowski@gmail.com>
2024-10-01 14:17:20 +0200

    add likes button

--- apps/frontend-websockets/src/pages/likes.tsx
+++ apps/frontend-websockets/src/pages/likes.tsx
@@ -1,15 +1,20 @@
 import React, { useEffect, useRef } from 'react';
 import { Box } from '@shared-ui/components/ui/box';
-import { BoxRow } from '@shared-ui/components/ui/box-row';
 import { io, Socket } from 'socket.io-client';
 import { Button } from '@shared-ui/components/ui/button';
 import { BoxColumn } from '@shared-ui/components/ui/boxColumn';
+import { PiThumbsDownThin, PiThumbsUpThin } from 'react-icons/pi';
+import { BoxRow } from '@shared-ui/components/ui/box-row';
 
-function LikesPage() {
+interface useSocketConnection {
+  url: string;
+}
+
+function useSocketConnection(data: useSocketConnection) {
   const socketRef = useRef<Socket | null>(null);
-
+  const { url } = data;
   useEffect(() => {
-    socketRef.current = io('http://localhost:4000');
+    socketRef.current = io(url);
 
     socketRef.current.on('connect', () => {
       console.log('@@@ connected ws');
@@ -20,23 +25,47 @@ function LikesPage() {
     };
   }, []);
 
-  socketRef.current?.emit('hello-word');
+  return { socket: socketRef };
+}
+
+function LikesPage() {
+  const { socket } = useSocketConnection({ url: 'http://localhost:4000' });
 
   return (
     <Box className={'w-screen h-screen flex justify-center items-center'}>
-      <BoxColumn className={'w-full justify-center items-center'}>
-        <BoxRow className={'h-fit w-4/5 justify-center bg-amber-300'}>
-          hello world
-        </BoxRow>
+      <BoxRow className={'justify-center items-center'}>
         <Button
+          className={
+            'rounded-l-xl rounded-r-0 border-r-0 rounded-tr-none rounded-br-none'
+          }
+          size={'lg'}
           variant="outline"
           onClick={() => {
-            socketRef.current?.emit('hello-test');
+            socket.current?.emit('hello-test');
           }}
         >
-          click me
+          <PiThumbsDownThin className={'text-2xl'} />
         </Button>
-      </BoxColumn>
+        <BoxRow
+          className={
+            'min-w-[85px] justify-center items-center border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground min-h-[40px] border-r-0 border-l-0'
+          }
+        >
+          hello
+        </BoxRow>
+        <Button
+          className={
+            'rounded-r-xl rounded-l-0 border-l-0 rounded-tl-none rounded-bl-none'
+          }
+          size={'lg'}
+          variant="outline"
+          onClick={() => {
+            socket.current?.emit('hello-test');
+          }}
+        >
+          <PiThumbsUpThin className={'text-2xl'} />
+        </Button>
+      </BoxRow>
     </Box>
   );
 }
