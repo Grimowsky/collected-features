b8e31afc0efe3cd2615c44aca898de0b7942ccc3
Grimm <rafalmirkowski@gmail.com>
2024-10-03 08:06:33 +0200

    generate random user id and redirect

--- apps/frontend-websockets/src/routes/AppRoutes.tsx
+++ apps/frontend-websockets/src/routes/AppRoutes.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { routes } from './routes.schema';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import { LikesPage } from '../pages/likes';
+
+function getRandomUserId(): number {
+  return Math.floor(Math.random() * 100) + 1;
+}
 
 export const AppRoutes = (): React.ReactElement => {
   return (
     <Routes>
       <Route path={`${routes.BASE_ROUTE}/:id`} element={<LikesPage />} />
+      <Route path="*" element={<Navigate to={`/${getRandomUserId()}`} />} />
     </Routes>
   );
 };
